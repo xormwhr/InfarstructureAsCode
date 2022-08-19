34b797dbc06186a8f755c0b4f107f1eb70eb4375
unknown <xormwhr@gmail.com>
2022-08-19 16:51:54 +0900

    cailco update

--- master_node.sh
+++ master_node.sh
@@ -2,7 +2,7 @@
 
 # init kubernetes 
 kubeadm init --token 123456.1234567890123456 --token-ttl 0 \
---pod-network-cidr=172.16.0.0/16 --apiserver-advertise-address=192.168.1.20
+--pod-network-cidr=172.17.0.0/16 --apiserver-advertise-address=192.168.1.20
 
 # config for master node only 
 mkdir -p $HOME/.kube

21708842a33ede9815cddf1ba081c26b979cbd8c
unknown <xormwhr@gmail.com>
2022-08-22 18:50:40 +0900

    2_install_pkg.sh update

--- 2_install_pkg.sh
+++ 2_install_pkg.sh
@@ -6,9 +6,9 @@ yum install vim-enhanced -y
 yum install git -y
 
 # install docker 
-yum install docker-ce docker-ce-cli containerd.io -y
+yum install docker -y
 systemctl enable --now docker
 
-# install kubernetes cluster 
+# install kubernetes cluster
 yum install kubectl kubelet kubeadm -y
 systemctl enable --now kubelet

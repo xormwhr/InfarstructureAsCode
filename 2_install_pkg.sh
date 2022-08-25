4cdea721503fe2ffb5543076ef1aba06ca4b5bdf
unknown <xormwhr@gmail.com>
2022-08-25 19:58:10 +0900

    version update

--- 2_install_pkg.sh
+++ 2_install_pkg.sh
@@ -1,14 +1,26 @@
 #!/usr/bin/env bash
 
+# yum list docker-ce --showduplicates | sort -r
+# yum list docker-ce-cli --showduplicates | sort -r
+# yum list containerd.io --showduplicates | sort -r
+
+# yum list kubectl --showduplicates | sort -r
+# yum list kubelet --showduplicates | sort -r
+# yum list kubeadm --showduplicates | sort -r
+
 # install packages 
 yum install epel-release -y
 yum install vim-enhanced -y
 yum install git -y
 
 # install docker 
-yum install docker -y
+yum install docker-ce-3:20.10.9-3.el7 \ 
+            docker-ce-cli-1:20.10.9-3.el7 \
+            containerd.io-1.6.7-3.1.el7 -y
 systemctl enable --now docker
 
 # install kubernetes cluster
-yum install kubectl kubelet kubeadm -y
+yum install kubectl-1.24.4-0 \
+            kubelet-1.24.4-0 \
+            kubeadm-1.24.4-0 -y
 systemctl enable --now kubelet

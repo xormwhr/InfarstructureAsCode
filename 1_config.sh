cc78cb5adf1ac8e52635a587fd11aa722d2c188f
unknown <xormwhr@gmail.com>
2022-08-29 18:37:26 +0900

    kube latest version upgrade

--- 1_config.sh
+++ 1_config.sh
@@ -1,17 +1,21 @@
 #!/usr/bin/env bash
 
+#######################################
 # vim configuration 
-# vim -> vi로 실행 별칭 (하이라이트로 보기 편함)
+# vim -> vi alias (highlight)
 echo 'alias vi=vim' >> /etc/profile
 
+#######################################
 # swapoff -a to disable swapping
-# swap 옵션 끄기
+# swap off
 swapoff -a
 # sed to comment the swap partition in /etc/fstab
-# swap에 관한 옵션 주석처리 -> 기존 옵션파일 .bak으로 생성
-# /swapfile none swap defaults 0 0 -> 주석처리
+# /etc/fstab -> /etc/fstab /etc/fstab.bak
+# /swapfile none swap defaults 0 0 -> # /swapfile none swap defaults 0 0
 sed -i.bak -r 's/(.+ swap .+)/#\1/' /etc/fstab
 
+#######################################
+# https://kubernetes.io/ko/docs/setup/production-environment/tools/kubeadm/install-kubeadm/
 # kubernetes repo
 gg_pkg="packages.cloud.google.com/yum/doc" # Due to shorten addr for key
 cat <<EOF > /etc/yum.repos.d/kubernetes.repo
@@ -24,40 +28,57 @@ repo_gpgcheck=0
 gpgkey=https://${gg_pkg}/yum-key.gpg https://${gg_pkg}/rpm-package-key.gpg
 EOF
 
+#######################################
+# https://docs.docker.com/engine/install/centos/
 # docker repo
 yum install yum-utils -y 
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 
+#######################################
+# https://kubernetes.io/ko/docs/setup/production-environment/tools/kubeadm/install-kubeadm/
 # Set SELinux in permissive mode (effectively disabling it)
-# SELinux 끄기 ( 보안 설정 )
+# SELinux off
 setenforce 0
 #enforcing -> permissive
 sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
 
+#######################################
 # firewalld off
-# 방화벽 끄기
 systemctl stop firewalld
+# firewalld systemd disable
 systemctl disable firewalld
 
-# RHEL/CentOS 7 have reported traffic issues being routed incorrectly due to iptables bypassed
+#######################################
+# https://kubernetes.io/ko/docs/setup/production-environment/container-runtimes/
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
+
+# br_netfilter 커널 모듈을 사용해 브리지로 네트워크를 구성
+# IP 마스커레이드(Masquerade)를 사용해 내부 네트워크와 외부 네트워크를 분리
+# IP 마스커레이드는 커널에서 제공하는 NAT(Network Address Translation)
+# br_netfilter를 적용함으로써 위의 iptables가 활성화
+modprobe br_netfilter
+modprobe overlay
+
 # 브리지 네트워크를 통과하는 IPv4와 IPv6의 패킷을 iptables가 관리하도록 설정
 # 파드의 통신을 iptables로 제어
 # 필요에 따라 IPVS(IP Virtual Server)같은 방식으로도 구성
 cat <<EOF >  /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward                 = 1
 EOF
-# br_netfilter 커널 모듈을 사용해 브리지로 네트워크를 구성
-# IP 마스커레이드(Masquerade)를 사용해 내부 네트워크와 외부 네트워크를 분리
-# IP 마스커레이드는 커널에서 제공하는 NAT(Network Address Translation)
-# br_netfilter를 적용함으로써 위의 iptables가 활성화
-modprobe br_netfilter
 
-# local small dns & vagrant cannot parse and delivery shell code.
-# 쿠버네티스 안에서 노드 간 통신을 이름으로 할 수 있도록 각 노드의 호스트 이름과 IP를 /etc/hosts에 설정
+sysctl --system
+
+#######################################
+# IP -> IP, Host name
 echo "192.168.1.20 m-k8s" >> /etc/hosts
 for (( i=1; i<=2; i++  )); do echo "192.168.1.20$i w$i-k8s" >> /etc/hosts; done
 
+#######################################
 # config DNS  
 cat <<EOF > /etc/resolv.conf
 nameserver 1.1.1.1 #cloudflare DNS
--- 2_install_pkg.sh
+++ 2_install_pkg.sh
@@ -8,15 +8,66 @@
 # yum list kubelet --showduplicates | sort -r
 # yum list kubeadm --showduplicates | sort -r
 
+#######################################
 # install packages 
 yum install epel-release -y
 yum install vim-enhanced -y
 yum install git -y
+# https://kubernetes.io/ko/docs/setup/production-environment/tools/kubeadm/install-kubeadm/
+yum install netcat -y
 
+#######################################
+# nfs install
+yum install nfs-utils -y
+
+#######################################
+# https://docs.docker.com/engine/install/centos/
 # install docker 
-yum install docker-ce-20.10.17-3.el7 docker-ce-cli-20.10.17-3.el7 containerd.io-1.6.7-3.1.el7 -y
-systemctl enable --now docker
+# yum install docker-ce-20.10.17-3.el7 docker-ce-cli-20.10.17-3.el7 containerd.io-1.6.7-3.1.el7 -y
+# kubernetes 1.24 ~ docker not used
+# containerd or CRI-O 
+yum install containerd.io-1.6.7-3.1.el7 -y
+# systemctl enable --now docker
 
+#######################################
+# https://kubernetes.io/docs/tasks/administer-cluster/kubeadm/configure-cgroup-driver/#modify-the-kubelet-configmap
+# kubernetes v1.22 ~  default(null) -> systemd
+
+# kubectl get cm kubeadm-config  -n kube-system -o yaml > kubeadm-config.yaml
+# cat <<EOF | /root/kubeadm-config.yaml
+# kind: KubeletConfiguration
+# apiVersion: kubelet.config.k8s.io/v1beta1
+# cgroupDriver: systemd
+# EOF
+
+# kubeadm init --config kubeadm-config.yaml
+
+# kubectl edit cm kubelet-config -n kube-system
+
+# kubectl edit add : cgroupDriver: systemd
+
+#######################################
+# https://github.com/containerd/containerd/blob/main/docs/getting-started.md
+# customiaing
+containerd config default > /etc/containerd/config.toml
+
+#######################################
+# https://kubernetes.io/ko/docs/setup/production-environment/container-runtimes/
+# systemd setting
+sed -i 's/SystemdCgroup = false/SystemdCgroup = true/' /etc/containerd/config.toml
+
+#######################################
+# https://docs.docker.com/engine/install/centos/
+# systemd add 
+systemctl enable --now containerd
+
+# ERRO[0000] unable to determine runtime API version:
+crictl config --set runtime-endpoint=unix:///run/containerd/containerd.sock
+# ERRO[0000] unable to determine image API version:
+crictl config --set image-endpoint=unix:///run/containerd/containerd.sock
+
+#######################################
 # install kubernetes cluster
-yum install kubectl-1.25.0-0 kubelet-1.25.0-0 kubeadm-1.25.0-0 -y
+# https://kubernetes.io/ko/docs/setup/production-environment/tools/kubeadm/install-kubeadm/
+yum install kubectl-1.25.0-0 kubelet-1.25.0-0 kubeadm-1.25.0-0 --disableexcludes=kubernetes -y
 systemctl enable --now kubelet
